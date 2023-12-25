2a875c1839a34720b8398363b8f60317b7515404
Bilal Fazlani <bilal.m.fazlani@gmail.com>
2023-12-25 23:33:22 +0530

    day 2 parsing works

--- src/main/scala/aoc/Day2.scala
+++ src/main/scala/aoc/Day2.scala
@@ -5,40 +5,57 @@ import zio.*
 import zio.stream.*
 import aoc.Challenge
 
-object Day2 extends Challenge[Int](day(2)):
+object Day2 extends Challenge[Seq[Game]](day(2)):
 
-  def execute: Task[Int] =
-    ZIO.succeed(0)
+  def parseLine(line: String) =
+    Game.parse(line)
+
+  def execute: Task[Seq[Game]] =
+    file
+      .map(parseLine)
+      .absolve
+      .runCollect
+      .mapError(e => Exception(e.toString))
 
 enum Color:
   case Blue, Red, Green
 case class CubeCount(count: Int, color: Color)
 case class Reveal(counts: Set[CubeCount])
 case class Game(id: Int, reveals: Seq[Reveal])
+object Game:
+  def parse(line: String) =
+    import GameSyntax.*
+    game.parseString(line)
 
 object GameSyntax {
   import zio.parser.*
   import zio.parser.Parser.*
 
-  val comma = char(',')
-  val semicolon = char(';')
-  def number = digit.repeat.map(_.mkString.toInt)
-  def color = string("blue", Color.Blue) | string("red", Color.Red) | string(
-    "green",
-    Color.Green
-  )
-  def cubeCount =
-    ((number zipLeft whitespace.repeat) ~ color).map(CubeCount.apply)
-  def reveal = cubeCount
-    .repeatWithSep(comma zipLeft whitespace.optional)
+  lazy val comma = char(',')
+  lazy val semicolon = char(';')
+
+  lazy val number = digit.repeat.map(_.mkString.toInt)
+
+  lazy val color =
+    (string("blue", Color.Blue) | string("red", Color.Red) | string(
+      "green",
+      Color.Green
+    )) ?? "color"
+
+  lazy val cubeCount =
+    ((number zip whitespace.optional.unit) ~ color).map(CubeCount.apply)
+
+  lazy val reveal = cubeCount
+    .repeatWithSep(comma zip whitespace.optional.unit)
     .map(x => Reveal(x.toSet))
-  def game =
+
+  lazy val game =
     ((string("Game", ()) ~ whitespace.unit ~ number ~ string(
       ":",
       ()
     ) ~ whitespace.unit)
-      ~ (reveal.repeatWithSep(semicolon zipLeft whitespace.optional)))
+      ~ (reveal.repeatWithSep(semicolon zip whitespace.optional.unit)))
       .map { case (id, reveals) =>
         Game(id, reveals)
-      }
+      } ~ end
 }
--- src/test/scala/aoc/Day2Test.scala
+++ src/test/scala/aoc/Day2Test.scala
@@ -6,9 +6,54 @@ import zio.stream.*
 import zio.test.Assertion.*
 
 object Day2Tests extends ZIOSpecDefault {
-  val spec = suite("Day 2 Tests")(
+  val spec = suite("Day 2 Syntax Tests")(
+    test("parse color") {
+      import GameSyntax.*
+      val input = "red"
+      val expected = Color.Red
+      val actual = color.parseString(input)
+      assertTrue(actual == Right(expected))
+    },
+    test("parse number") {
+      import GameSyntax.*
+      val input = "34"
+      val expected = 34
+      val actual = number.parseString(input)
+      assertTrue(actual == Right(expected))
+    },
+    test("parse cube count") {
+      import GameSyntax.*
+      val input = "34 red"
+      val expected = CubeCount(34, Color.Red)
+      val actual = cubeCount.parseString(input)
+      assertTrue(actual == Right(expected))
+    },
+    test("parse reveal") {
+      import GameSyntax.*
+      val input = "34 red, 12 blue"
+      val expected =
+        Reveal(Set(CubeCount(34, Color.Red), CubeCount(12, Color.Blue)))
+      val actual = reveal.parseString(input)
+      assertTrue(actual == Right(expected))
+    },
+    test("parse game") {
+      import GameSyntax.*
+      val input = "Game 12: 34 red, 12 blue; 12 green, 34 red; 2 red"
+      val expected = Game(
+        12,
+        Seq(
+          Reveal(Set(CubeCount(34, Color.Red), CubeCount(12, Color.Blue))),
+          Reveal(Set(CubeCount(12, Color.Green), CubeCount(34, Color.Red))),
+          Reveal(Set(CubeCount(2, Color.Red)))
+        )
+      )
+      val actual = game.parseString(input)
+      assertTrue(actual == Right(expected))
+    }
+  ) + suite("Day 2 Tests")(
     test("cubes game") {
-      Day2.execute.map(value => assertTrue(value == 0))
+      Day2.execute
+        .map(_ => assertCompletes)
     }
   )
 }
