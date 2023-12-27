1086d077a7df933a6dec8d3015498f4aafaf22ac
Bilal Fazlani <bilal.m.fazlani@gmail.com>
2023-12-27 23:25:09 +0530

    add caching . still not working

--- src/main/scala/aoc/day4/Part2.scala
+++ src/main/scala/aoc/day4/Part2.scala
@@ -2,23 +2,32 @@ package aoc
 package day4
 
 import scala.annotation.tailrec
+import scala.collection.mutable
 
 object Part2 extends Challenge(day(4)):
   def execute =
     val cards = input.map(Card.parse)
 
+    val cache = mutable.Map.empty[Int, Int]
 
     @tailrec
     def processCard(i: Int, pendingIndexes: List[Int], count: Int): Int = {
-      val card = cards(i)
-      val matchCount = card.matchCount
+      cache.get(i) match
+        case None =>
+          val card = cards(i)
+          val matchCount = card.matchCount
 
-      val nextCount = matchCount min (cards.length - 1 - i)
-      val nextIndexes = Range(i + 1, i + nextCount).inclusive.toList
-      val totalIndexes = pendingIndexes ++ nextIndexes
-      if totalIndexes.nonEmpty
-      then processCard(totalIndexes.head, totalIndexes.tail, count = count + 1)
-      else count + 1
+          val nextCount = matchCount min (cards.length - 1 - i)
+          val nextIndexes = Range(i + 1, i + nextCount).inclusive.toList
+          val totalIndexes = pendingIndexes ++ nextIndexes
+          if totalIndexes.nonEmpty
+          then processCard(totalIndexes.head, totalIndexes.tail, count = count + 1)
+          else count + 1
+        case Some(cached) => count + cached
     }
 
-    Range(0, cards.length).map(i => processCard(i, Nil, 0)).sum
+    Range(0, cards.length).map { i =>
+      val out = processCard(i, Nil, 0)
+      cache.update(i, out)
+      out
+    }.sum
