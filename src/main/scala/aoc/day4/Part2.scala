1a4a58b0dd8cd0343f0049bb123b49e128346ca3
Bilal Fazlani <bilal.m.fazlani@gmail.com>
2023-12-28 16:06:44 +0530

    solved day 4 part 2

--- src/main/scala/aoc/day4/Part2.scala
+++ src/main/scala/aoc/day4/Part2.scala
@@ -2,24 +2,35 @@ package aoc
 package day4
 
 import scala.annotation.tailrec
+import scala.collection.mutable
 
 object Part2 extends Challenge(day(4)):
   def execute =
     val cards = input.map(Card.parse)
 
-    @tailrec
+    val cache = mutable.Map.empty[Int, Int]
+
     def processCard(indexes: List[Int], count: Int): Int = {
       val (i, pending) = (indexes.head, indexes.tail)
-      val card = cards(i)
-      val matchCount = card.matchCount
-      val remainingCards = cards.length - i - 1
+      if cache.contains(i) && pending.isEmpty
+      then count + cache(i)
+      else if cache.contains(i)
+      then processCard(pending, count + cache(i))
+      else
+        val card = cards(i)
+        val matchCount = card.matchCount
+        val remainingCards = cards.length - i - 1
 
-      val nextCount = matchCount min remainingCards
-      if nextCount > 0 || pending.nonEmpty then
+        val nextCount = matchCount min remainingCards
         val nextIndexes = Range(i + 1, i + nextCount).inclusive.toList
         val totalIndexes = pending ++ nextIndexes
-        processCard(totalIndexes, count + 1)
-      else count + 1
+        if totalIndexes.nonEmpty then
+          val result = processCard(totalIndexes, count + 1)
+          cache.update(i, result)
+          result
+        else
+          cache.update(i, count + 1)
+          count + 1
     }
 
-    Range(0, cards.length).map(i => processCard(List(i), 0)).sum
+    (0 to cards.length - 1).reverse.map(i => processCard(List(i), 0)).sum
