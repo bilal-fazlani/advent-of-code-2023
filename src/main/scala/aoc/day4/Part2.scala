16d64e721f137ec6bf360a3337ffcbe1283280db
Bilal Fazlani <bilal.m.fazlani@gmail.com>
2023-12-28 16:43:54 +0530

    convert challenge 4.2 to functional

--- src/main/scala/aoc/day4/Part2.scala
+++ src/main/scala/aoc/day4/Part2.scala
@@ -1,36 +1,23 @@
 package aoc
 package day4
 
-import scala.annotation.tailrec
-import scala.collection.mutable
-
 object Part2 extends Challenge(day(4)):
   def execute =
     val cards = input.map(Card.parse)
 
-    val cache = mutable.Map.empty[Int, Int]
+    def process(card: Card, index: Int, cache: Map[Int, Int]): Map[Int, Int] =
+      val matchCount = card.matchCount
+      val remainingCards = cards.length - index - 1
 
-    def processCard(indexes: List[Int], count: Int): Int = {
-      val (i, pending) = (indexes.head, indexes.tail)
-      if cache.contains(i) && pending.isEmpty
-      then count + cache(i)
-      else if cache.contains(i)
-      then processCard(pending, count + cache(i))
-      else
-        val card = cards(i)
-        val matchCount = card.matchCount
-        val remainingCards = cards.length - i - 1
+      val nextCount = matchCount min remainingCards
+      val nextIndexes = Range(index + 1, index + nextCount).inclusive.toList
 
-        val nextCount = matchCount min remainingCards
-        val nextIndexes = Range(i + 1, i + nextCount).inclusive.toList
-        val totalIndexes = pending ++ nextIndexes
-        if totalIndexes.nonEmpty then
-          val result = processCard(totalIndexes, count + 1)
-          cache.update(i, result)
-          result
-        else
-          cache.update(i, count + 1)
-          count + 1
-    }
+      val theirCount = nextIndexes.map(cache(_)).sum
+      cache.updated(index, theirCount + 1)
 
-    (0 to cards.length - 1).reverse.map(i => processCard(List(i), 0)).sum
+    cards.zipWithIndex
+      .foldRight((Map.empty[Int, Int])) { case ((card, index), cache) =>
+        process(card, index, cache)
+      }
+      .map(_._2)
+      .sum
